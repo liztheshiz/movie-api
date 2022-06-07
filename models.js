cf6d69f86f615f65ad36b62617862f22396f873c
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-07 09:22:23 -0400

    initialized models.js file

---
+++ models.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: {type: String, required: true},
+    Description: {type: String, required: true},
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String,
+        Birth: Date,
+        Death: Date
+    },
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
