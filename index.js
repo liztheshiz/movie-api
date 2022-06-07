1c55c6e34459e6a9d922fc3ca22b842ca9177086
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-07 12:09:00 -0400

    updated response message in delete movie method

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.delete('/users/:username/FavoriteMovies/:movieid', (req, res) => {
     Users.findOneAndUpdate({ Username: req.params.username }, {
         $pull: { FavoriteMovies: req.params.movieid }
     })
-    .then(user => {res.status(201).send('Movie ' + req.params.title + ' was deleted from ' + user.name + '\'s list.')})
+    .then(user => {res.status(201).send('Movie ' + req.params.movieid + ' was deleted from ' + user.Username + '\'s list.')})
     .catch(err => {
         console.error(err);
         res.status(500).send('Error: ' + err);
