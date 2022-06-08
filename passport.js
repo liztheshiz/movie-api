05e42e7b2bb937d667b53a38f4f9542373a60cc5
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-08 11:03:07 -0400

    initialized passport.js file

---
+++ passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJWT;
+
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+}, (username, password, callback) => {
+    console.log(username + '  ' + password);
+    Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+            console.log(error);
+            return callback(error);
+        }
+
+        if (!user) {
+            console.log('incorrect username');
+            return callback(null, false, {message: 'Incorrect username or password.'});
+        }
+
+        console.log('finished');
+        return callback(null, user);
+    });
+}));
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+  }, (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
+        .then((user) => {
+            return callback(null, user);
+        })
+        .catch((error) => {
+            return callback(error)
+        });
+  }));
