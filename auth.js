2a331a35dbc6ecc6800a6e995fb11ed8b6afa309
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-08 11:15:11 -0400

    fixed indentation

--- auth.js
+++ auth.js
@@ -18,17 +18,17 @@ module.exports = (router) => {
     router.post('/login', (req, res) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
             if (error || !user) {
-            return res.status(400).json({
-                message: 'Something is not right',
-                user: user
-            });
+                return res.status(400).json({
+                    message: 'Something is not right',
+                    user: user
+                });
             }
             req.login(user, { session: false }, (error) => {
-            if (error) {
-                res.send(error);
-            }
-            let token = generateJWTToken(user.toJSON());
-            return res.json({ user, token });
+                if (error) {
+                    res.send(error);
+                }
+                let token = generateJWTToken(user.toJSON());
+                return res.json({ user, token });
             });
         })(req, res);
     });
