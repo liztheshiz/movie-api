20900bcb97d7fe4b8532847c925f2bb6f626417f
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-09 10:13:01 -0400

    added methods to hash passwords

--- models.js
+++ models.js
@@ -26,6 +26,14 @@ let userSchema = mongoose.Schema({
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+}
+
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
 
