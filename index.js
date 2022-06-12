f82f6662414a3980bdd58dfa6f2bb3e1d93d5b02
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-11 21:02:36 -0400

    hashed updated password

--- index.js
+++ index.js
@@ -132,10 +132,11 @@ app.put('/users/:username', passport.authenticate('jwt', {session: false}),
             return res.status(422).json({errors: errors.array()});
         }
 
+        let hashedPassword = Users.hashPassword(req.body.Password);
         Users.findOneAndUpdate({ Username: req.params.username }, { $set:
             {
                 Username: req.body.Username,
-                Password: req.body.Password,
+                Password: hashedPassword,
                 Email: req.body.Email,
                 Birthday: req.body.Birthday
             }
