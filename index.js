993c9e75bb145c96bb0c41f0c8ea305c7c697a80
liztheshiz <65947305+liztheshiz@users.noreply.github.com>
2022-06-15 09:14:42 -0400

    updated formatting (automatic)

--- index.js
+++ index.js
@@ -23,15 +23,14 @@ app.use(express.static('public'));
 let allowedOrigins = ['http://localhost:8081', 'https://cinemadatabase.herokuapp.com', 'http://localhost:1234'];
 app.use(cors({
     origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
             let message = 'The CORS policy for this application doesn\’t allow access from origin ' + origin;
-            return callback(new Error(message ), false);
+            return callback(new Error(message), false);
         }
         return callback(null, true);
     }
 }));
-*/
 
 let auth = require('./auth.js')(app);
 const passport = require('passport');
